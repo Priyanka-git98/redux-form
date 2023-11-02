3ddc4665a2896e25309f8dffcde2a8d6f7b982a0
Priyanka-git98 <148323876+Priyanka-git98@users.noreply.github.com>
2023-11-02 17:09:50 +0530

    redux store initialized

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import store from './redux/store';
-import MyForm from './components/Registration';
+import MyForm from './components/Signup';
 
 function App() {
   return (
