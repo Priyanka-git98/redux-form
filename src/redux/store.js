2e0c1536533e5dac301bab19f733be16253e8a9b
Priyanka-git98 <148323876+Priyanka-git98@users.noreply.github.com>
2023-11-07 15:15:50 +0530

    middleware applied

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,10 @@
-import { legacy_createStore as createStore } from 'redux';
+import { legacy_createStore as createStore, applyMiddleware  } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import logger from 'redux-logger'
 import rootReducer from './rootReducer'
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, composeWithDevTools(
+    applyMiddleware(logger),
+));
 
 export default store;
